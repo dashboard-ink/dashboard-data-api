3d46d78d9835c327c79b57a42588bfbf8defb2e2
Dovydas Venckus <dovydas.venckus@live.com>
2021-07-01 23:21:38 +0300

    Init spring boot kotlin app

---
+++ src/test/kotlin/dev/dovydasvenckus/dashboarddataapi/DashboardDataApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.dovydasvenckus.dashboarddataapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DashboardDataApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
